783742aa31154866c6dccc870b8ced0248c1f419
Protonosgit <linuslinz98@gmail.com>
2024-06-11 22:10:19 +0200

    added broken listener

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,15 @@
 import { Slot } from 'expo-router';
 import React, { useEffect } from 'react';
-
+import messaging from '@react-native-firebase/messaging';
 
 function Root() {
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('Notification received in the foreground!', remoteMessage);
+    });
+    return unsubscribe;
+  }, []);
 
   return (
       <Slot />
